5041b15fef327f06ff04cbf24dadb8a7807de863
Svetlana2282 <93007753+Svetlana2282@users.noreply.github.com>
2021-11-12 10:03:23 +0200

    Add files via upload

---
+++ Лр4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	float Z, y, e;
+	cout << "input y\n";
+	cin >> y;
+	Z = 1;
+	for (int k = 1; k <= 10; k++) {
+		Z = k + 2 * Z;
+	}
+	Z = log(y);
+	cout << "Z=" << setprecision(4) << Z << endl;
+	system("pause");
+	return 0;
+
+}
+
+
+
